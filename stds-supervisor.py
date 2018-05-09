7c079f0750bc9178d583fc2f5e84a5ba503b2152
rrbb014 <gutssoul1@gmail.com>
2018-05-09 19:09:51 +0900

    modify: get 3 problems

--- stds-supervisor.py
+++ stds-supervisor.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 	with open('stds-problems.txt', 'r', encoding='utf8') as f:
 		problems = [p for p in f.read().split('\n') if p != '']
 	
-	for _ in range(10):
+	for _ in range(3):
 		p = random.choice(problems)
 		problems.remove(p)
 		print(p)
