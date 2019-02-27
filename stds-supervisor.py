331f3fba25f23626ce0ab3bae9dc566577f5b3ec
rrbb014 <gutssoul1@gmail.com>
2019-02-27 12:18:25 +0900

    ADD argparse for control input file and number of problems

--- stds-supervisor.py
+++ stds-supervisor.py
@@ -1,10 +1,20 @@
+import os
 import random
+import argparse
+
 
 if __name__ == "__main__":
-	with open('stds-problems.txt', 'r', encoding='utf8') as f:
-		problems = [p for p in f.read().split('\n') if p != '']
-	
-	for _ in range(3):
-		p = random.choice(problems)
-		problems.remove(p)
-		print(p)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-f', '--file', default='stds-problems.txt', type=str, help='Problem file')
+    parser.add_argument('-n', '--number', type=int, default=3, help='Number of problem')
+
+    args = parser.parse_args()
+
+    with open(args.file, 'r', encoding='utf8') as f:
+        problems = [p for p in f.read().split('\n') if p != '']
+
+    for _ in range(args.number):
+        p = random.choice(problems)
+        problems.remove(p)
+        print(p)
