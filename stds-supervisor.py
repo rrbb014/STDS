525fd77ff441672f430b2bcf814bb092e29f6abd
rrbb014 <gutssoul1@gmail.com>
2020-08-24 21:30:28 +0900

    handle all lines

--- stds-supervisor.py
+++ stds-supervisor.py
@@ -8,13 +8,18 @@ if __name__ == "__main__":
 
     parser.add_argument('-f', '--file', default='stds-problems.txt', type=str, help='Problem file')
     parser.add_argument('-n', '--number', type=int, default=3, help='Number of problem')
+    parser.add_argument('-a', '--all', action='store_true', help='handle all lines')
 
     args = parser.parse_args()
 
     with open(args.file, 'r', encoding='utf8') as f:
         problems = [p for p in f.read().split('\n') if p != '']
 
-    for _ in range(args.number):
+    num = args.number
+    if args.all:
+        num = len(problems)
+
+    for _ in range(num):
         p = random.choice(problems)
         problems.remove(p)
         print(p)
